c75e6dfa6b76d5f3fae88973c89ed5efeca248c1
githubwqj2 <2812930196@qq.com>
2018-06-07 18:14:03 +0800

    笔记本提交

---
+++ src/com/wqj/scala/grammar/methodScala.scala
@@ -0,0 +1,20 @@
+package wqj.scala.grammar
+
+/**
+  * @Auther: wqj
+  * @Date: 2018/6/7 18:01
+  * @Description:
+  */
+object methodScala {
+
+  def m1(a:Int,b:Int):Int = {
+
+
+//    return a*b
+     a*b
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(m1(3,4))
+  }
+}
