5fe283eb3727c05dc82c3cf6723fc0407d6220e5
githubwqj <893453172@qq.com>
2018-06-09 16:54:19 +0800

    台式机提交

--- src/com/wqj/scala/grammar/SingleWordCount.scala
+++ src/com/wqj/scala/grammar/SingleWordCount.scala
@@ -32,6 +32,7 @@ object SingleWordCount {
     val b=List(1,2,3,4,5,6,7)
 //    println(b.toArray.reduce(_+_))
 //    println(b.fold(10)(_+_))
+//    println(b.toArray.par.reduce(_+_))
 //    println(b.par.fold(0)(_+_))
 //    //并行计算
 //    println(b.par.fold(10)(_+_))
@@ -47,10 +48,33 @@ object SingleWordCount {
 //    println(c2)
 
     val c3=c2.toArray.sortBy(_._2).reverse.map(t => (t._2,t._1))
-    print(c3.toBuffer)
+//    print(c3.toBuffer)
 
 //    val c3=lines.flatMap(_.split(" ")).map((_,1)).groupBy(_._1).mapValues(t =>t.reduce()
 
 //    print(c3)
+
+
+
+
+    //聚合
+//    val arr = List(List(1, 2, 3), List(3, 4, 5), List(2), List(0))
+//    print(arr.par.aggregate(0)(_+_.sum,_+_))
+
+
+    val l1 = List(5,6,4,7)
+    val l2 = List(1,2,3,4)
+
+    //求并集 (元素会重复)
+    println(l1.union(l2))
+    //求交集
+    println(l1.intersect(l2))
+    //求差集
+    println(l1.diff(l2))
+
+
+
+
+
   }
 }
